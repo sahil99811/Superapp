d1b54e9151d6afcb53a0a2853e86364c9ffce7f0
sahil99811 <sp220253@gmail.com>
2024-03-06 19:42:48 +0530

    fourth commit

--- src/components/news/News.jsx
+++ src/components/news/News.jsx
@@ -6,21 +6,22 @@ const DEFAULT_IMAGE ="https://static.foxnews.com/foxnews.com/content/uploads/202
 
 const News = () => {
   const [news, setNews] = useState(null);
-
+   
   const fetchNews = async () => {
+  
     try {
       const response = await fetch(
-        `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
+        "https://api.worldnewsapi.com/search-news?text=cricket&language=en&api-key=ee60a78608e847d0b46024a661eea384"
       );
       if (!response.ok) {
         throw new Error("Failed to fetch news");
       }
       const data = await response.json();
-      console.log(data)
-      const articleNo = Math.floor(Math.random() * data.articles.length);
-      const selectedArticle = data.articles[articleNo];
+     
+      const articleNo = Math.floor(Math.random() *data.news.length);
+      const selectedArticle = data.news[articleNo];
       console.log(selectedArticle)
-      const formattedDate = formatDate(selectedArticle.publishedAt);
+      const formattedDate = formatDate(selectedArticle.publish_date);
       selectedArticle.publishedAt = formattedDate;
       setNews(selectedArticle);
     } catch (error) {
@@ -48,9 +49,8 @@ const News = () => {
     <>
       {news && (
         <>
-        
-          <img className="news-img" src={news.urlToImage || DEFAULT_IMAGE} alt="news" />
-          <p className="news-content">{news.content}</p>
+          <img className="news-img" src={news.image || DEFAULT_IMAGE} alt="news" />
+          <p className="news-content">{news.text}</p>
           <div className="news-title">
             <h2>{news.title || "Title not available"}</h2>
             <span>{news.publishedAt || "Publication date not available"}</span>
